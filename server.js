6d3f55fa23db13c8d74197b27739d34c33d1e285
Branden <b.lowe333@gmail.com>
2018-06-13 15:47:46 -0700

    added project hbs file and updated header to add a project a el

--- server.js
+++ server.js
@@ -59,6 +59,10 @@ app.get('/bad', (req, res) => {
   }
   res.send(returnMSg)
 })
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs')
+})
 app.listen(port, () => {
   console.log(`server is up on port ${port}`)
 })
